a4aafeb924107b00ff87cbe8aeb3f64addb52d13
Dave Frame <dframe@industrydive.com>
2023-06-12 14:29:14 -0600

    more setup settings

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(
     install_requires=[
         'google-cloud-bigquery >= 2.30.1',
         'pytest >= 7.3.0',
-    ]
+    ],
+    name='django-bigquery-exporter',
+    packages=['bigquery_exporter'],
+    version='0.1.0',
 )
